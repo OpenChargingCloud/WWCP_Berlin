ffeb240f63868c17fc77eed1666d434c88737d70
Achim Friedland <github@ahzf.de>
2016-12-02 02:18:05 +0100

    updates

---
+++ WWCP_Berlin/BerlinStations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+using Newtonsoft.Json.Linq;
+
+namespace WWCP_Berlin
+{
+
+    public static class BerlinStations
+    {
+
+        public static JObject ParseJSON(String Text)
+        {
+
+            var BerlinJSON = JObject.Parse(Text);
+
+            return new JObject(
+                       new JProperty("type",       "FeatureCollection"),
+                       new JProperty("properties", new JObject()),
+                       new JProperty("features",   new JArray(
+                           BerlinJSON["list"].Select(item => {
+
+                               var InfoObject = item as JObject;
+
+                               return new JObject(
+                                          new JProperty("type", "Feature"),
+                                          new JProperty("properties", new JObject(
+                                              new JProperty("Id",       InfoObject["pointId"]),
+                                              new JProperty("Operator", InfoObject["operatorId"])
+                                          )),
+                                          new JProperty("geometry", new JObject(
+                                              new JProperty("type", "Point"),
+                                              new JProperty("coordinates", new JArray(InfoObject["x"], InfoObject["y"]))
+                                          ))
+
+                                   );
+
+                           }
+                       )))
+                   );
+
+        }
+
+    }
+
+}
